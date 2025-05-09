e81b2f9606848768463b05678095ade90b77cb67
Mattia PARRINELLO <contactperso.mp@gmail.com>
2025-05-09 17:17:55 +0200

    V1

--- server.js
+++ server.js
@@ -6,13 +6,39 @@ const fetch = require('node-fetch');
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 
 const PORT = process.env.PORT || 3000;
-const OPENWEATHER_API_KEY = 'b2d38bf82ded340fa00aba84a9b75373'; // Remplace par ta clé
-const VILLE = 'Paris';
-const LASTFM_API_KEY = '567072b8078523239f50efc119474c0c';
+const OPENWEATHER_API_KEY = process.env.OPENWEATHER_API_KEY
+const VILLE = 'Franconville, 95130' // Ville par défaut;
+const VILLE_SHORT = 'Franconville' // Ville courte pour l'affichage
+const LASTFM_API_KEY = process.env.LASTFM_API_KEY
 
-
+function convertVilleToCoords(ville) {
+    // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+    try {
+        const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ville}&limit=1&appid=${OPENWEATHER_API_KEY}`;
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                if (data.length > 0) {
+                    const { lat, lon } = data[0];
+                    console.log(`Coordonnées de ${ville}: Latitude: ${lat}, Longitude: ${lon}`);
+                    return [lat, lon];
+                } else {
+                    console.error('Ville non trouvée');
+                }
+            })
+            .catch(err => console.error('Erreur:', err));
+    }
+    catch (err) {
+        console.error('Erreur:', err);
+    }
+    return [0, 0]; // Valeurs par défaut si la ville n'est pas trouvée
+}
 let currentConfig = {
     showTime: true,
     showWeather: true,
@@ -38,21 +64,50 @@ const weatherIconMap = {
     '01n': 'clear_night',
     '02d': 'partly_cloudy_day',
     '02n': 'partly_cloudy_night',
-    '03d': 'cloud',
-    '03n': 'cloud',
-    '04d': 'cloud',
-    '04n': 'cloud',
-    '09d': 'rainy',
-    '09n': 'rainy',
-    '10d': 'rainy',
-    '10n': 'rainy',
-    '11d': 'thunderstorm',
-    '11n': 'thunderstorm',
-    '13d': 'snowing',
-    '13n': 'snowing',
-    '50d': 'foggy',
-    '50n': 'foggy'
-};
+    '03d': 'mostly_cloudy_day',
+    '03n': 'mostly_cloudy_night',
+    '04d': 'cloudy',
+    '04n': 'cloudy',
+    '09d': 'showers_rain',
+    '09n': 'showers_rain',
+    '10d': 'rain_with_cloudy_dark',
+    '10n': 'rain_with_cloudy_dark',
+    '11d': 'strong_thunderstorms',
+    '11n': 'strong_thunderstorms',
+    '13d': 'snow_with_cloudy_dark',
+    '13n': 'snow_with_cloudy_dark',
+    '50d': 'haze_fog_dust_smoke',
+    '50n': 'haze_fog_dust_smoke',
+    '12d': 'scattered_showers_day',
+    '12n': 'scattered_showers_night',
+    '14d': 'isolated_thunderstorms',
+    '14n': 'isolated_thunderstorms',
+    '15d': 'sleet_hail',
+    '15n': 'sleet_hail',
+    '16d': 'snow_with_rain_dark',
+    '16n': 'snow_with_rain_dark',
+    '17d': 'blizzard',
+    '17n': 'blizzard',
+    '18d': 'drizzle',
+    '18n': 'drizzle',
+    '19d': 'flurries',
+    '19n': 'flurries',
+    '20d': 'scattered_snow_showers_day',
+    '20n': 'scattered_snow_showers_night',
+    '21d': 'heavy_snow',
+    '21n': 'heavy_snow',
+    '22d': 'mixed_rain_hail_sleet',
+    '22n': 'mixed_rain_hail_sleet',
+    '23d': 'isolated_scattered_thunderstorms_day',
+    '23n': 'isolated_scattered_thunderstorms_night',
+    '24d': 'scattered_showers_day',
+    '24n': 'scattered_showers_night',
+    '25d': 'cloudy_with_rain_dark',
+    '25n': 'cloudy_with_rain_dark',
+    '26d': 'cloudy_with_snow_dark',
+    '26n': 'cloudy_with_snow_dark'
+}
+
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
@@ -64,20 +119,50 @@ app.get('/screen', (req, res) => {
 
 app.get('/api/weather', async (req, res) => {
     try {
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=${VILLE}&units=metric&lang=fr&appid=${OPENWEATHER_API_KEY}`;
+        const [lat, lon] = convertVilleToCoords(VILLE);
+        const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&lang=fr&appid=${OPENWEATHER_API_KEY}`;
+        console.log('URL:', url);
         const response = await fetch(url);
         const data = await response.json();
         const iconCode = data.weather[0].icon;
         const materialIcon = weatherIconMap[iconCode] || 'cloud';
 
         res.json({
-            name: data.name,
+            name: VILLE_SHORT,
             temp: Math.round(data.main.temp),
             desc: data.weather[0].description,
             icon: materialIcon
         });
     } catch (e) {
+        console.error('Erreur récupération météo:', e);
         res.status(500).json({ error: 'Erreur météo' });
+    }
+});
+
+app.get("/api/forecast", async (req, res) => {
+    console.log("Requête prévisions météo reçue");
+    try {
+        const [lat, lon] = convertVilleToCoords(VILLE);
+        const url = `https://api.openweathermap.org/data/2.5/forecast/?lat=${lat}&lon=${lon}&units=metric&appid=${OPENWEATHER_API_KEY}`;
+        const response = await fetch(url);
+        const data = await response.json();
+
+        const simplified = data.list.slice(0, 5).map(f => {
+            const iconCode = f.weather[0].icon;
+            const materialIcon = weatherIconMap[iconCode] || 'cloud';
+
+            return {
+                dt: f.dt,
+                temp: Math.round(f.main.temp),
+                description: f.weather[0].description,
+                icon: materialIcon
+            };
+        });
+
+        res.json({ list: simplified });
+    } catch (err) {
+        console.error("Erreur prévisions météo :", err);
+        res.status(500).json({ error: "err" });
     }
 });
 
@@ -117,12 +202,12 @@ app.post('/api/music', async (req, res) => {
     res.sendStatus(200);
 });
 
-
 io.on('connection', (socket) => {
     socket.emit('displayContent', currentConfig);
 
     socket.on('updateContent', (data) => {
         currentConfig = { ...currentConfig, ...data };
+        console.log('Configuration mise à jour:', currentConfig);
         io.emit('displayContent', currentConfig);
     });
 });
